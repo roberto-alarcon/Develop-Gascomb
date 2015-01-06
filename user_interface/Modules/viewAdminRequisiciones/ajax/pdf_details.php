31618b1d5c8022620e8c2658c1dc50ecf748aac4
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-01-06 17:30:00 -0600

    .

--- user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
+++ user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
@@ -3,5 +3,6 @@ include '/home/gascomb/secure_html/config/set_variables.php';
 include_once (PATH_CLASSES_FOLDER.'class.inventory_control_pdf.php');
 
 // Instanciamos la clase
+// Segunda linea brach master
 $PDF = new Services_PDF('28866');
 $PDF->creaPDF();
