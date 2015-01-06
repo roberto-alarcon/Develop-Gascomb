7ca7e3e80e373423d3afb3098815a78a9eec855c
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-01-06 17:12:28 -0600

    Segunda linea de Código

--- user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
+++ user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
@@ -3,5 +3,6 @@ include '/home/gascomb/secure_html/config/set_variables.php';
 include_once (PATH_CLASSES_FOLDER.'class.inventory_control_pdf.php');
 
 // Instanciamos la clase
+// Cambiar folio ID por un metodo get 
 $PDF = new Services_PDF('28866');
 $PDF->creaPDF();
