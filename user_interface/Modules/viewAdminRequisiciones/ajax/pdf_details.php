13b9102700dc8d592ef3dff2b286968d16ffa46d
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-01-06 17:09:14 -0600

    Agregamos linea de comentarios

--- user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
+++ user_interface/Modules/viewAdminRequisiciones/ajax/pdf_details.php
@@ -2,5 +2,6 @@
 include '/home/gascomb/secure_html/config/set_variables.php';
 include_once (PATH_CLASSES_FOLDER.'class.inventory_control_pdf.php');
 
+// Instanciamos la clase
 $PDF = new Services_PDF('28866');
 $PDF->creaPDF();
