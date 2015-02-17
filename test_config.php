6fde4c565608710c82b8e586595ae2c3579f2add
Roberto Alarcón García <roberto.alarcon@tours360.com.mx>
2015-02-17 14:14:59 -0600

    Archivo de configuracion
    Se incluye archivo test con el cual podremos saber con que
    configuración esta operando la aplicación

---
+++ test_config.php
@@ -0,0 +1,27 @@
+<?php
+include_once('/home/gascomb/secure_html/config/set_variables.php');
+
+echo "<h3>Sistema</h3>";
+echo "Dominio -> ".DOMAIN."<br>";
+echo "Path Libreria -> ".PATH_DHTMLX_LIBRARY."<br>";
+echo "Path Ajax -> ".PATH_USER_INTERFACE_AJAX."<br>";
+echo "Path Multimedia -> ".PATH_MULTIMEDIA."<br>";
+echo "Dir Multimedia -> ".PATH_MULTIMEDIA_BASE."<br>";
+echo "Dir Aplicacion -> ".PATH_BASE_FOLDER."<br>";
+
+
+echo "Dir Clases -> ".PATH_CLASSES_FOLDER."<br>";
+echo "Path Multimedia -> ".URL_MULTIMEDIA."<br>";
+echo "Path QR -> ".QR_IMAGE_URL."<br>";
+echo "Path URL PDF -> ".PDF_URL."<br>";
+echo "Path Imagen -> ".PATH_IMAGE_INVENTORY."<br>";
+echo "Path Server -> ".PATH_SERV."<br>";
+// BD // 
+echo "<h3>BD</h3>";
+echo "BD USER -> ".BD_USER."<br>";
+echo "BD PASS -> ".BD_PASSWORD."<br>";
+echo "BD DATABASE -> ".BD_DATABASE."<br>";
+echo "BD SERVER -> ".BD_SERVER."<br>";
+
+
+?>
