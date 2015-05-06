8673fa2a66ab3500468afb14638e89c6a46c83c0
Roberto Alarcón García <roberto.alarcon@tours360.com.mx>
2015-05-05 19:01:29 -0500

    Agregamos validación para el host name git

--- config/set_variables.php
+++ config/set_variables.php
@@ -16,7 +16,8 @@ if ( $_SERVER['SERVER_NAME'] == 'localhost' ){
 	include_once('host/localhost.php');
 }else{
 	// Main Site
-	include_once('host/sistema.gascomb.com.php');
+	die("No existe una configuracion para el host ".$_SERVER['SERVER_NAME']);
+	//include_once('host/sistema.gascomb.com.php');
 }
 //echo PATH_CLASSES_FOLDER."class.gascomb.php";exit(0);
 //include_once (PATH_CLASSES_FOLDER."class.gascomb.php");
