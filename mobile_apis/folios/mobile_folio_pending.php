64dd0bc341c1d17747b5234f0f76ae1a1fce7582
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-06-12 12:06:25 -0500

    mobile_folio:pending resultado vacio

--- mobile_apis/folios/mobile_folio_pending.php
+++ mobile_apis/folios/mobile_folio_pending.php
@@ -9,18 +9,26 @@ $folios_mobile = $folio_mobile->selectAllPending();
 $ipad_pending = array();
 
 #echo "<pre>"; print_r($folios_mobile); echo "</pre>"; die();
-foreach ($folios_mobile as $clave => $valor) {
-			    $qrcode = "";
-				$dependency = "";
-				$dependency = $folio_mobile->getDependency($valor['dependency_id']);
-				$qrcode = $folio_mobile->getUrlqrcode($valor["folio_id"]);
-				
-				$pad_pending[$clave]['folio'] = $valor["folio_id"];
-				$pad_pending[$clave]['url_qrcode'] = $qrcode;
-				$pad_pending[$clave]['dependency'] = utf8_encode($dependency);
-				$pad_pending[$clave]['plate'] = $valor['registration_plate'];
-				$pad_pending[$clave]['company'] = COMPANY;				
-}
+if(isset($folios_mobile) && $folios_mobile != null && count($folios_mobile) > 0){
+	foreach ($folios_mobile as $clave => $valor) {
+		$qrcode = "";
+		$dependency = "";
+		$dependency = $folio_mobile->getDependency($valor['dependency_id']);
+		$qrcode = $folio_mobile->getUrlqrcode($valor["folio_id"]);
+		
+		$pad_pending[$clave]['folio'] = $valor["folio_id"];
+		$pad_pending[$clave]['url_qrcode'] = $qrcode;
+		$pad_pending[$clave]['dependency'] = utf8_encode($dependency);
+		$pad_pending[$clave]['plate'] = $valor['registration_plate'];
+		$pad_pending[$clave]['company'] = COMPANY;				
+	}
+} else {
+	$pad_pending[0]['folio'] = "";
+	$pad_pending[0]['url_qrcode'] = "";
+	$pad_pending[0]['dependency'] = "";
+	$pad_pending[0]['plate'] = "";
+	$pad_pending[0]['company'] = "";	    	
+}	
 #echo "<pre>"; print_r($pad_pending); echo "</pre>"; die();
 echo json_encode($pad_pending);
 
