ef37bf8bb8728fe36dad34f6b9c4a27913a11aca
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-06-28 12:45:29 -0500

    cambio 3 pts multimedia

--- config/host/pts.gascomb.com.php
+++ config/host/pts.gascomb.com.php
@@ -2,8 +2,8 @@
 error_reporting(-1);
 ini_set('display_errors', '1');
 define("DOMAIN", "http://pts.gascomb.com/");
-define("COMPANY", "Professional Technician Services S.A. de C.v");
-define("NAME_COMPANY", "GRUPO AUTOMOTRIZ EN SERVICIOS DE COMBUSTIBLE");
+define("COMPANY", "Professional Technician Services S.A. de C.V.");
+define("NAME_COMPANY", "Professional Technician Services S.A. de C.V.");
 define("PATH_DHTMLX_LIBRARY", "http://pts.gascomb.com/dhtmlxLibrary/");
 define("PATH_USER_INTERFACE_AJAX", "http://pts.gascomb.com/ajax/");
 define("PATH_USER_INTERFACE_AJAX_PDF", "/home/gascomb/pts_secure_html/Develop-Gascomb/user_interface/ajax/");
@@ -17,7 +17,6 @@ define("PDF_URL",PATH_MULTIMEDIA."[id_folio]/pdf/[id_folio].pdf");
 define("PATH_IMAGE_INVENTORY", "/multimedia/inventory/");
 define("PATH_SERV", PATH_BASE_FOLDER);
 
-
 // BD //
 define("BD_USER", "root");
 define("BD_PASSWORD", "AAKBgQCtNFZpXIDoab00ce0BeVe5Jqjgc+");
@@ -25,7 +24,7 @@ define("BD_DATABASE", "sistema_ptservice");
 define("BD_SERVER", "localhost");
 
 //header && nom
-$headerpdf = '<span class="title_emp">GRUPO MECANICO EMPRESARIAL, S.A DE C.V</span><br/><br/>
+$headerpdf = '<span class="title_emp">Professional Technician Services S.A. de C.V.</span><br/><br/>
 			<span class="text_small">LABORATORIO DIESEL INTEGRAL PARA TODAS LAS MARCAS Y TIPOS DE BOMBAS E INYECTORES, LABORATORIO PARA INYECTORES ELECTRÓNICOS HUEI, EUI: RECONSTRUCCIONES INTEGRALES Y MÉCANICA EN GENERAL EN GASOLINA DIESEL Y MAQUINARIA PESADA FUEL INYECTION Y DIAGNOSTICOS POR COMPUTADORA ESPECIALIZADO EN EQUIPO PESADO</span><br/>
 			<span class="text_small">R.F.C. GME-951206-9K1   GME@AVANTE.NET<br/>
 			TELS. 2693-2370, 5693-2276, 5693-4367</span>';
