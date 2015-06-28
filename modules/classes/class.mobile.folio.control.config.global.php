240a39cb05bf4faea65a28fbc300f4c746eed6b3
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-06-28 13:18:45 -0500

    changes mobile_pending_global instancias

--- modules/classes/class.mobile.folio.control.config.global.php
+++ modules/classes/class.mobile.folio.control.config.global.php
@@ -5,7 +5,8 @@
 	error_reporting(-1);
 	define("BD_USER_GASCOMB", "root");
 	define("BD_PASSWORD_GASCOMB", "AAKBgQCtNFZpXIDoab00ce0BeVe5Jqjgc+");
-	define("BD_DATABASE_GASCOMB", "github_gascomb");
+	define("BD_DATABASE_GITHUB", "github_gascomb");
+	define("BD_DATABASE_GASCOMB", "sistema_gascomb");
 	define("BD_SERVER", "localhost");
 	define("BD_DATABASE_PTS", "sistema_ptservice");
 	define("BD_DATABASE", "sistema_gascomb");
