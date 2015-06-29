03314080babae248d9b4e07b0af3c7fde46fc1b8
Roberto Alarcon <roberto.alarcon@tours360.com.mx>
2015-06-29 11:53:05 -0500

    cache pending global

--- mobile_apis/folios/mobile_folio_pending_global.php
+++ mobile_apis/folios/mobile_folio_pending_global.php
@@ -1,6 +1,14 @@
 <?php
 ini_set('display_errors', '1');
 header('Content-Type: application/json');
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+   
+
 include_once ('../../modules/classes/class.mobile.folio.global.php');
 
 $folio_mobile = new FoliosMobiles;
